112a98ffaf46125aec991988610da065e109bd4a
miguelbrieva <miguel.brieva@audiomusica.com>
2019-04-29 15:25:22 -0400

    12 Writing a Todo List Reducer (Toggling a Todo)

--- src/index.js
+++ src/index.js
@@ -13,6 +13,16 @@ const todos = (state = [], action) => {
           completed: false,
         },
       ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
     default:
       return state;
   }
@@ -37,5 +47,40 @@ const testAddTodo = () => {
   expect(todos(stateBefore, action)).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1,
+  };
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false,
+    },
+  ];
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true,
+    },
+  ];
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('All test passed.');
