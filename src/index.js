19fd97b5ad1ef53fb676d5d503cb7d347dae1404
miguelbrieva <miguel.brieva@audiomusica.com>
2019-04-29 16:09:19 -0400

    13 Reducer Composition with Arrays

--- src/index.js
+++ src/index.js
@@ -2,27 +2,34 @@ const deepFreeze = require('deep-freeze');
 const expect = require('expect');
 
 // Reducer
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false,
+      };
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state;
+      }
+      return {
+        ...state,
+        completed: !state.completed,
+      };
+    default:
+      return state;
+  }
+};
+
+// Reducer
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [
-        ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false,
-        },
-      ];
+      return [...state, todo(undefined, action)];
     case 'TOGGLE_TODO':
-      return state.map(todo => {
-        if (todo.id !== action.id) {
-          return todo;
-        }
-        return {
-          ...todo,
-          completed: !todo.completed,
-        };
-      });
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
