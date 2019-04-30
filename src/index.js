3705b3e2a64f95ef3b6478ae0f24b0c2314d0fb8
miguelbrieva <miguel.brieva@audiomusica.com>
2019-04-30 15:23:16 -0400

    14 Reducer Composition with Objects

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-const deepFreeze = require('deep-freeze');
-const expect = require('expect');
+const { createStore } = require('redux');
 
-// Reducer
 const todo = (state, action) => {
   switch (action.type) {
     case 'ADD_TODO':
@@ -23,7 +21,6 @@ const todo = (state, action) => {
   }
 };
 
-// Reducer
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
@@ -35,59 +32,62 @@ const todos = (state = [], action) => {
   }
 };
 
-const testAddTodo = () => {
-  const stateBefore = [];
-  const action = {
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux',
-  };
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false,
-    },
-  ];
-  deepFreeze(stateBefore);
-  deepFreeze(action);
-  expect(todos(stateBefore, action)).toEqual(stateAfter);
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
+    default:
+      return state;
+  }
 };
 
-const testToggleTodo = () => {
-  const action = {
-    type: 'TOGGLE_TODO',
-    id: 1,
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(state.todos, action),
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
   };
-  const stateBefore = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false,
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: false,
-    },
-  ];
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false,
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: true,
-    },
-  ];
-  deepFreeze(stateBefore);
-  deepFreeze(action);
-  expect(todos(stateBefore, action)).toEqual(stateAfter);
 };
 
-testAddTodo();
-testToggleTodo();
-console.log('All test passed.');
+const store = createStore(todoApp);
+
+console.log('Initial state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux',
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping',
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching TOGGLE_TODO');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0,
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER');
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED',
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
