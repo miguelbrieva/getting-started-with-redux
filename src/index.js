132e41ab6b9732d132ab9f367dfadc44174ed9fb
miguelbrieva <miguel.brieva@audiomusica.com>
2019-05-07 14:57:04 -0400

    17 React Todo List Example (Adding a Todo)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const { createStore } = require('redux');
-// const { createStore, combineReducers } = require('redux');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
 
 const todo = (state, action) => {
   switch (action.type) {
@@ -42,25 +43,6 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
   }
 };
 
-// Reduce a group of reducers. This is a very common pattern.
-// const todoApp = (state = {}, action) => {
-//   return {
-//     todos: todos(state.todos, action),
-//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-//   };
-// };
-
-// Reimplementation of combineReducers()
-const combineReducers = reducers => {
-  return (state = {}, action) => {
-    return Object.keys(reducers).reduce((nextState, key) => {
-      nextState[key] = reducers[key](state[key], action);
-      return nextState;
-    }, {});
-  };
-};
-
-// Generrate a reducer from several other reducers intead of writing it by hand
 const todoApp = combineReducers({
   todos,
   visibilityFilter,
@@ -68,63 +50,40 @@ const todoApp = combineReducers({
 
 const store = createStore(todoApp);
 
-console.log('Initial state:');
-console.log(store.getState());
-console.log('--------------');
+let nextTodoId = 0;
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <input ref={node => (this.input = node)} />
+        <button
+          onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              text: this.input.value,
+              id: nextTodoId++,
+            });
+            this.input.value = '';
+          }}
+        >
+          Add Todo
+        </button>
+        <ul>
+          {this.props.todos.map(todo => (
+            <li key={todo.id}>{todo.text}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 0,
-  text: 'Learn Redux',
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
+const render = () => {
+  ReactDOM.render(
+    <TodoApp todos={store.getState().todos} />,
+    document.getElementById('root')
+  );
+};
 
-console.log('Dispatching ADD_TODO.');
-store.dispatch({
-  type: 'ADD_TODO',
-  id: 1,
-  text: 'Go shopping',
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching TOGGLE_TODO');
-store.dispatch({
-  type: 'TOGGLE_TODO',
-  id: 0,
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-console.log('Dispatching SET_VISIBILITY_FILTER');
-store.dispatch({
-  type: 'SET_VISIBILITY_FILTER',
-  filter: 'SHOW_COMPLETED',
-});
-console.log('Current state:');
-console.log(store.getState());
-console.log('--------------');
-
-// /*******************************************************************
-//  * The reduce() method executes a reducer function (that you provide)
-//  * on each member of the array resulting in a single output value*/
-// const array1 = [1, 3, 5];
-// const reducer = (accumulator, currentValue) => accumulator + currentValue;
-// console.log(array1.reduce(reducer)); // 9
-// /*******************************************************************/
-
-// /*******************************************************************
-//  * Object.keys() method return an array of a given object's own
-//  * property names, in the same order a we get with a normal loop*/
-// const object1 = {
-//   a: 'somestring',
-//   b: 42,
-//   c: false,
-// };
-// console.log(Object.keys(object1)); // ["a", "b", "c"]
-// /*******************************************************************/
+store.subscribe(render);
+render();
